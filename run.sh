b09cdcc4a122663c70d415e372506873b025e0cf
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-07-28 10:31:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-./spectre-miner   --mining-address=spectre:qzexxs4q0lkg5pnvpqlxrwugznlqs2eef9gmekv3hfr0uc2mvxz2c4rc8wj6u  --spectred-address=68.183.190.249:443 -t 7
+./spectre-miner --mining-address=spectre:qzexxs4q0lkg5pnvpqlxrwugznlqs2eef9gmekv3hfr0uc2mvxz2c4rc8wj6u --spectred-address=68.183.190.249:443 -t 7
 pause
